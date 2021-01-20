22dc8b74d91820ab284f60a1a33fd815ce0ffe17
Jimbo <marquardtjames@gmail.com>
2021-01-20 00:29:00 -0600

    dead comment removal

--- poems/limericks.py
+++ poems/limericks.py
@@ -1,33 +1,5 @@
-# import syllables
 import json
 import random
-
-# fakes = json.load(open('fake_messages_with_known_words.json', 'r'))
-
-
-# by_count = {}
-
-# eight_by_last_2_syllables = {}
-# five_by_last_2_syllables = {}
-# for fake in fakes:
-#     if not fake:
-#         continue
-#     count = syllables.sentence_syllables(fake)
-#     last_2_syllables = "+".join(syllables.vowel_phones_for_last_n_syllables(fake,2))
-
-#     if(count == 8):
-#         if last_2_syllables in eight_by_last_2_syllables:
-#             eight_by_last_2_syllables[last_2_syllables].append(fake)
-#         else:
-#             eight_by_last_2_syllables[last_2_syllables] = [fake]
-#     if(count == 5):
-#         if last_2_syllables in five_by_last_2_syllables:
-#             five_by_last_2_syllables[last_2_syllables].append(fake)
-#         else:
-#             five_by_last_2_syllables[last_2_syllables] = [fake]
-
-
-# remove lines that are less than 3 words
 
 def remove_short_lines(syllable_lookup, min_length=3):
     for syllable_count in list(syllable_lookup.keys()):
@@ -36,13 +8,6 @@ def remove_short_lines(syllable_lookup, min_length=3):
             if len(by_syllable[key]) < min_length:
                 del by_syllable[key]
     return syllable_lookup
-
-# for key in five_by_last_2_syllables.keys():
-#     if len(five_by_last_2_syllables[key]) < 3:
-#         del five_by_last_2_syllables[key]
-
-
-
 
 def limerick(syllable_lookup):
     a_ending_syllables = random.choice(list(syllable_lookup[8].keys()))
